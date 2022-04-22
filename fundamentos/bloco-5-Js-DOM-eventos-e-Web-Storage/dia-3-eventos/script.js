29efbb0c243eb6a7424fe978acde33d384607524
BrandtLC <lucasbrandtcosta@gmail.com>
2022-04-22 17:49:04 -0300

    resolve exercicio 2

--- fundamentos/bloco-5-Js-DOM-eventos-e-Web-Storage/dia-3-eventos/script.js
+++ fundamentos/bloco-5-Js-DOM-eventos-e-Web-Storage/dia-3-eventos/script.js
@@ -21,15 +21,15 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-function addDays() {
-  const dezDaysList = [
-    29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-  ];
+const dezDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
+function addDays(mounth) {
   const mounthDaysList = document.querySelector("#days");
 
-  for (let index = 0; index < dezDaysList.length; index += 1) {
-    const mounthDays = dezDaysList[index];
+  for (let index = 0; index < mounth.length; index += 1) {
+    const mounthDays = mounth[index];
     const mounthDaysListItem = document.createElement("li");
     mounthDaysListItem.innerHTML = mounthDays;
 
@@ -37,4 +37,18 @@ function addDays() {
   }
 }
 
-addDays();
+addDays(dezDaysList);
+
+const buttonsCountainer = document.querySelector(".buttons-container");
+
+function createButton() {
+  const button = document.createElement("button");
+  const buttonText = document.createElement("h3");
+  buttonText.innerHTML = "Feriados";
+  button.appendChild(buttonText);
+  button.id = 'btn-holiday';
+
+  buttonsCountainer.appendChild(button);
+}
+
+createButton();
